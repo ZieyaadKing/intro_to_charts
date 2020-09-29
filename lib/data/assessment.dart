c3a38f5aee6f787d7b810d178a94de5954fcae8f
ZieyaadKing <k.m.zieyaad@gmail.com>
2020-09-30 00:52:58 +0200

    Created a model class that stores assessment data.

---
+++ lib/data/assessment.dart
@@ -0,0 +1,10 @@
+// Assesment:
+// A model class for handling indexing and marks for each student
+
+class Assessment {
+
+  final String name;
+  final int marks;
+
+  Assessment({this.name, this.marks});
+}
