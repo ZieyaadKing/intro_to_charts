c37db8c6c7f4358a3b06a4d4b58732f7b739f466
ZieyaadKing <k.m.zieyaad@gmail.com>
2020-09-30 00:44:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Intro to charts',
+      theme: ThemeData.light(),
+      home: HomePage(title: 'Intro to charts'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
